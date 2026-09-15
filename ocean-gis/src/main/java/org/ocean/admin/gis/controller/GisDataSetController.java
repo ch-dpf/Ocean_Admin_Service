dafa47d481367abce27961d65b3b1e6fe3e20c99
dupf <dpf1357595059@163.com>
2026-09-15 18:50:34 +0800

    feat(gis):地理信息服务功能整合

--- ocean-gis/src/main/java/org/ocean/admin/gis/controller/GisDataSetController.java
+++ ocean-gis/src/main/java/org/ocean/admin/gis/controller/GisDataSetController.java
@@ -24,7 +24,7 @@ import java.util.List;
 @RequestMapping("/api/gis/dataSet")
 @Tag(name = "GIS数据集管理", description = "GIS数据集管理接口")
 @RequiredArgsConstructor
-public class DataSetController {
+public class GisDataSetController {
 
     private final GisDataSetService gisDataSetService;
 
